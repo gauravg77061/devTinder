d6c708b6127f99091de6ca101352daafd6d166ff
gaurav <gauravg77061@gmail.com>
2025-12-06 13:46:36 +0530

    updation of base url for dev and localhost

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,8 @@
-export const BASE_URL= 'api/';
+
+// this is for production
+
+//export const BASE_URL= 'api/';
+
+// this is for dev enviornment 
+
+export const BASE_URL= location.hostname === "localhost" ?'http://localhost:3000/' : "api/";
